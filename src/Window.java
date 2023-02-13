3b6bea00bea14f714e3d3eade2df31c3fe912ac2
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-02-13 10:57:57 +0100

    init

--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class window {
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+
+    public Window (){
+        this.setTitle("click me");
+        this.setPreferredSize(new Dimension(300,300));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(new Square());
+
+        //finish up
+        this.pack();
+        this.setVisible(true);
+    }
 }
